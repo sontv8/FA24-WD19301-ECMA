73236710ddd637120db37f8c782e3a6b0ab7ac1d
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-11-29 10:25:53 +0700

    update product

--- my-app/src/pages/Admin/UpdateProduct.jsx
+++ my-app/src/pages/Admin/UpdateProduct.jsx
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateProduct = () => {
   const [inputValues, setInputValues] = useState({});
+
+  const id = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:3000/products/${id.id}`)
+      .then((response) => response.json())
+      .then((data) => setInputValues(data));
+  }, []);
+
   const onHandleChange = (e) => {
     const { name, value } = e.target;
     console.log(name, value);
@@ -15,18 +24,34 @@ const UpdateProduct = () => {
   return (
     <div>
       <h1>Cập nhật sản phẩm</h1>
+      {JSON.stringify(inputValues)}
       <form onSubmit={onHandleSubmit}>
         <div>
           <label htmlFor="">Tên sản phẩm</label>
-          <input type="text" onInput={onHandleChange} name="name" />
+          <input
+            type="text"
+            onInput={onHandleChange}
+            name="name"
+            defaultValue={inputValues.name}
+          />
         </div>
         <div>
           <label htmlFor="">Giá sản phẩm</label>
-          <input type="text" name="price" onInput={onHandleChange} />
+          <input
+            type="text"
+            name="price"
+            onInput={onHandleChange}
+            defaultValue={inputValues.price}
+          />
         </div>
         <div>
           <label htmlFor="">Ảnh sản phẩm</label>
-          <input type="text" name="image" onInput={onHandleChange} />
+          <input
+            type="text"
+            name="image"
+            onInput={onHandleChange}
+            defaultValue={inputValues.image}
+          />
         </div>
         <button>Cập nhật</button>
       </form>
